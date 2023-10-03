39c850019758777c65883cd3ff5208165730a91f
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2023-10-03 11:51:16 -0300

    Add files via upload

---
+++ typenumber.js
@@ -0,0 +1,15 @@
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaomatematica =primeiroNumero+segundoNumero;
+console. log(operacaomatematica)
+const text = "nome mimi"
+
+//ponto flutuante 
+const numeroflutuante = 0.3;
+const numeroflutantesemzero= 0.5;
+const novaoperacao = primeiroNumero/numeroflutuante;
+console.log(novaopercao);
+//nan not a number (nao e numero)
+const testenan="testenan";
+console.log(primeiroNumero * testenan);
